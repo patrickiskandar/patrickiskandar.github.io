93d3d1470290aced058dfca17f8fd9c6d5c825e0
Patrick Iskandar <42042336+patrickiskandar@users.noreply.github.com>
2021-09-23 14:52:02 -0400

    Delete patientchk.sql

--- patientchk.sql
+++
@@ -1,8 +0,0 @@
-SELECT o.freetext, h.name, o.date_time
-FROM observations o, healthcare_professionals h
-WHERE o.freetext LIKE '%breathing%'
-AND o.hid = h.hid
-AND o.patientid IN
-  (SELECT patientid FROM patients
-  WHERE name = 'Samantha Adam'
-);;
