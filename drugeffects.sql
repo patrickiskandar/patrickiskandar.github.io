67cc73a432c1d44921778b02d650968e09c95175
Patrick Iskandar <42042336+patrickiskandar@users.noreply.github.com>
2021-09-23 14:51:39 -0400

    Delete drugeffects.sql

--- drugeffects.sql
+++
@@ -1,7 +0,0 @@
-SELECT COUNT(DISTINCT o.patientid)
-FROM observations o, drugs d
-WHERE d.name = 'tocilizumab'
-AND d.patientid = o.patientid
-AND o.date_time > MIN(d.date_time)
-AND o.freetext LIKE '%dizz%'
-;;
